a1afcedf36e70b9f41338a00225ec9a0e04e759b
Miguel Rincón <ma.rinconr1@uniandes.edu.co>
2023-11-05 11:44:58 -0500

    Se implementa el servicio

--- src/app/vehiculo/vehiculo.component.ts
+++ src/app/vehiculo/vehiculo.component.ts
@@ -1,21 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import { Vehiculo } from './vehiculo';
+import { VehiculoService } from './vehiculo.service';
 
 @Component({
   selector: 'app-vehiculo',
   templateUrl: './vehiculo.component.html',
   styleUrls: ['./vehiculo.component.css']
 })
+
 export class VehiculoComponent implements OnInit {
 
+  
+  constructor(private vehiculoService: VehiculoService) { }
   vehiculos: Array<Vehiculo> = [];
-  constructor() { }
-  getVehiculoList(): Array<Vehiculo> {
-    return [];
+
+  getVehiculos() {
+    this.vehiculoService.getVehiculos().subscribe(vehiculos => {
+      this.vehiculos = vehiculos;
+    });
   }
 
   ngOnInit() {
-    this.vehiculos = this.getVehiculoList();
+    this.getVehiculos();
   }
 
 }
--- src/app/vehiculo/vehiculo.service.ts
+++ src/app/vehiculo/vehiculo.service.ts
@@ -1,11 +1,21 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Vehiculo } from './vehiculo';
+
+import { environment } from 'src/environments/environment';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class VehiculoService {
+  private apiUrl = environment.baseUrl;
 
 constructor(private http: HttpClient) { }
+
+  getVehiculos():Observable<Vehiculo[]> {
+    return this.http.get<Vehiculo[]>(this.apiUrl);
+  }
 
 }

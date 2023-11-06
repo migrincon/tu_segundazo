fc3e32060b1f55e02890a9e2340e6432758271c7
Miguel Rincón <ma.rinconr1@uniandes.edu.co>
2023-11-05 22:58:21 -0500

    release: 1.0.0: ParcialMISW4104

---
+++ src/app/vehiculo/contador-marca.ts
@@ -0,0 +1,12 @@
+export class ContadorMarca {
+    marca: string;
+    contador: number;
+
+    public constructor(marca: string, contador: number)
+    {
+        this.marca = marca;
+        this.contador = contador;
+
+
+    }
+}
